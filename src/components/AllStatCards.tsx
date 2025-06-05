50814cdc43e534c028ba6475f40ad14793e10414
“Osaro <saro.iyoha@gmail.com>
2025-06-05 18:43:16 +0100

    upadte AllStatCards.tsx

--- src/components/AllStatCards.tsx
+++ src/components/AllStatCards.tsx
@@ -9,15 +9,15 @@ function AllStatCards() {
   return (
     <div className={`${themeClass} all_cards`}>
         <div className="card card__total">
-            <h2>{characterCount}</h2>
+            <h2>{characterCount<10? `0${characterCount}`:characterCount}</h2>
             <p>Total Characters</p>
         </div>
         <div className="card card__word-count">
-            <h2>{wordCount}</h2>
+            <h2>{wordCount<10? `0${wordCount}`:wordCount}</h2>
             <p>Word Count</p>
         </div>
         <div className="card card__sentence-count">
-            <h2>{sentenceCount}</h2>
+            <h2>{sentenceCount<10? `0${sentenceCount}`:sentenceCount}</h2>
             <p>Sentence Count</p>
         </div>
     </div>
